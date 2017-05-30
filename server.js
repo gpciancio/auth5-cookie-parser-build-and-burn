0dcf4d60eb612b196d824ad31433b363e47904e5
gpciancio <gopalciancio@gmail.com>
2017-05-30 14:05:40 -0600

    complete

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get('/set', (req,res,next) => {
-  
+  res.cookie("foo", "bar").send();
 });
 
 app.get('/read', (req,res,next) => {
-
+  var newCookie = req.cookies;
+  res.send(newCookie);
 });
 
 app.get('/clear', (req,res,next) => {
+res.clearCookie('deleteMe').send();
 
 });
 
